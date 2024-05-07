f047883aee8529699f9a5ee66cbfe5d47f8fdf67
Giovanni Baratta <bargiovi@hotmail.it>
2024-05-07 15:32:22 +0200

    add car component for enter car expenses details

---
+++ src/components/CarComponent.tsx
@@ -0,0 +1,91 @@
+import {Paper, styled} from "@mui/material"
+import {CurrencyInputField} from "./shared/CurrencyInputField.tsx"
+import React, {useEffect, useState} from "react"
+import {Either} from "fp-ts/Either"
+import {either} from "fp-ts"
+import {NumericInputField} from "./shared/NumericInputField.tsx"
+import {
+  setDuration as setDurationAction,
+  setMonthlyRateAction,
+  setStartPaymentDateAction,
+  setUpfrontAction
+} from "../state/car/car-reducer.ts"
+import {useAppDispatch} from "../state/store.ts"
+import {DatePicker} from "@mui/x-date-pickers"
+import dayjs, {Dayjs} from "dayjs"
+import {getFirstDayOfNextMonthsFrom} from "../utils/date.ts"
+
+interface CarProps {
+  disabled: boolean
+}
+
+const DURATION_DEFAULT_VALUE = 24
+const MONTHLY_RATE_DEFAULT_VALUE = 400
+const UPFRONT_DEFAULT_VALUE = 5000
+
+export const CarComponent: React.FC<CarProps> = (props: CarProps) => {
+
+  const datePickerMinDate = getFirstDayOfNextMonthsFrom(new Date(), 1)
+
+  const dispatch = useAppDispatch()
+  const [duration, setDuration] = useState<number>(DURATION_DEFAULT_VALUE)
+  const [monthlyRate, setMonthlyRate] = useState<number>(MONTHLY_RATE_DEFAULT_VALUE)
+  const [upfront, setUpfront] = useState<number>(UPFRONT_DEFAULT_VALUE)
+  const [startPayingFrom, setStartPayingFrom] = useState<Date>(datePickerMinDate)
+
+  const onDurationChange = (value: number) => {
+    setDuration(value)
+  }
+
+  const onMonthlyRateChange = (value: number) => {
+    setMonthlyRate(value)
+  }
+
+  const onUpfrontChange = (value: number) => {
+    setUpfront(value)
+  }
+
+  const onStartPaymentFromChange = (value: Dayjs | null) => {
+    if (value !== null)
+      setStartPayingFrom(value.toDate())
+  }
+
+  useEffect(() => {
+    dispatch(setDurationAction(duration))
+  }, [dispatch, duration])
+
+  useEffect(() => {
+    dispatch(setMonthlyRateAction(monthlyRate))
+  }, [dispatch, monthlyRate])
+
+  useEffect(() => {
+    dispatch(setUpfrontAction(upfront))
+  }, [dispatch, upfront])
+
+  useEffect(() => {
+    dispatch(setStartPaymentDateAction(startPayingFrom.toDateString()))
+  }, [dispatch, startPayingFrom])
+
+  return (
+    <Paper elevation={1}>
+      <Title>Car</Title>
+      <CurrencyInputField label={"Monthly Payment"} onValueChange={onMonthlyRateChange}
+                          validate={validateGreaterOrEqualToZero} defaultValue={monthlyRate}
+                          disabled={props.disabled} />
+      <CurrencyInputField label={"Upfront Payment"} onValueChange={onUpfrontChange}
+                          validate={validateGreaterOrEqualToZero} defaultValue={upfront} disabled={props.disabled} />
+      <DatePicker defaultValue={dayjs(datePickerMinDate)} label="Start payment from" views={["month", "year"]}
+                  minDate={dayjs(datePickerMinDate)} onChange={onStartPaymentFromChange} disabled={props.disabled} />
+      <NumericInputField label={"Duration"} validate={validateGreaterOrEqualToZero} defaultValue={duration}
+                         onValueChange={onDurationChange} disabled={props.disabled} />
+    </Paper>
+  )
+}
+
+const validateGreaterOrEqualToZero = (value: number): Either<string, true> => value >= 0 ? either.right(true) : either.left("Value must be greater or equal to 0")
+
+const Title = styled("div")(() => ({
+  padding: "10px",
+  fontWeight: "bold",
+  textAlign: "left"
+}))
