88693c2e0b30b8bf54a5e9a5dee8c355ce4a1067
Giovanni Baratta <bargiovi@hotmail.it>
2024-05-13 18:28:16 +0200

    Add summary, costs projections and income components to App
    Signed-off-by: Giovanni Baratta <bargiovi@hotmail.it>

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import "./App.css"
 import {CarComponent} from "./components/CarComponent.tsx"
 import {CostsProjectionComponent, Report} from "./components/CostsProjectionComponent.tsx"
-import {Button, TextField} from "@mui/material"
+import {Grid, TextField} from "@mui/material"
 import React, {useState} from "react"
 import {useAppSelector} from "./state/store.ts"
 import {getFirstDayOfNextMonthsFrom} from "./utils/date.ts"
 import {buildCarExpensesCalculator} from "./model/car.ts"
+import {SummaryComponent} from "./components/SummaryComponent.tsx"
+import {IncomeComponent} from "./components/IncomeComponent.tsx"
+import {buildIncomeCalculator, GrowthOptions} from "./model/income.ts"
 
 const SIMULATION_LENGTH_IN_YEARS = 10
 const SIMULATION_RESOLUTION_IN_MONTHS = 1
@@ -15,10 +18,7 @@ function App() {
   const [simulationLengthInYears, setSimulationLengthInYears] = useState(SIMULATION_LENGTH_IN_YEARS)
   const [simulationResolutionInMonths, setSimulationResolutionInMonth] = useState(SIMULATION_RESOLUTION_IN_MONTHS)
   const carState = useAppSelector(state => state.car)
-
-  const refreshCostProjection = () => {
-    console.log(carState)
-  }
+  const incomeState = useAppSelector(state => state.income)
 
   const simulationLengthOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const value = event.target.value
@@ -47,16 +47,34 @@ function App() {
     upfront: carState.upfrontPayment
   })
 
+  const growth: GrowthOptions = incomeState.yoyGrowth.type === "GrowthDisable" ? {
+    type: "disabled"
+  } : {
+    type: "yearly",
+    annualPercentageGrowth: incomeState.yoyGrowth.percentage,
+    startDate: simulationStartDate
+  }
+
+  const incomeCalculator = buildIncomeCalculator({
+    monthlyNetRate: incomeState.monthlyIncomeRate,
+    quattordicesima: incomeState.quattordicesima,
+    tredicesima: incomeState.tredicesima,
+    growth
+  })
+
   while (simulationCurrentDate.getTime() < simulationEndingDate.getTime()) {
 
-    const carReport = carExpensesCalculator.computeMonthlyReport({
+    const period = {
       month: simulationCurrentDate.getMonth(),
       year: simulationCurrentDate.getFullYear()
-    })
+    }
+
+    const carReport = carExpensesCalculator.computeMonthlyReport(period)
+    const incomeReport = incomeCalculator.computeMonthlyReport(period)
 
     const record: Report = {
       date: simulationCurrentDate,
-      income: 0,
+      income: incomeReport.totalIncome,
       totalMonthExpenses: carReport.totalExpenses
     }
 
@@ -64,20 +82,26 @@ function App() {
     simulationCurrentDate = getFirstDayOfNextMonthsFrom(simulationCurrentDate, simulationResolutionInMonths)
   }
 
-  console.log(records)
-
   return (
     <>
-      <CarComponent disabled={false} />
-      <TextField label={"Number of years"} defaultValue={simulationLengthInYears} type={"number"}
-                 onChange={simulationLengthOnChange} />
-      <TextField label={"Resolution (months)"} defaultValue={simulationResolutionInMonths} type={"number"}
-                 onChange={simulationResolutionOnChange} />
-      <Button variant="contained"
-              onClick={refreshCostProjection}>Refresh
-        projection</Button>
-      <div>Start date: {simulationStartDate.toDateString()} End date: {simulationEndingDate.toDateString()}</div>
-      <CostsProjectionComponent data={records} />
+      <Grid container spacing={2} padding={2}>
+        <Grid xs={6}>
+          <IncomeComponent />
+          <CarComponent disabled={false} />
+
+        </Grid>
+        <Grid xs={4}>
+          <SummaryComponent records={records} />
+        </Grid>
+        <Grid xs={12} padding={2}>
+          <TextField label={"Number of years"} defaultValue={simulationLengthInYears} type={"number"}
+                     onChange={simulationLengthOnChange} />
+          <TextField label={"Resolution (months)"} defaultValue={simulationResolutionInMonths} type={"number"}
+                     onChange={simulationResolutionOnChange} />
+          <div>Start date: {simulationStartDate.toDateString()} End date: {simulationEndingDate.toDateString()}</div>
+          <CostsProjectionComponent data={records} />
+        </Grid>
+      </Grid>
     </>
   )
 }
