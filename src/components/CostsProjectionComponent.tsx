09a1ea51faab0a4619232f8e21c07fb897503932
Giovanni Baratta <bargiovi@hotmail.it>
2024-05-13 18:26:23 +0200

    Show balance column in costs projection Change formatting of currency values
    Signed-off-by: Giovanni Baratta <bargiovi@hotmail.it>

--- src/components/CostsProjectionComponent.tsx
+++ src/components/CostsProjectionComponent.tsx
@@ -1,5 +1,6 @@
 import {Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@mui/material"
 import React from "react"
+import {formatNumberToEuro} from "../utils/print.ts"
 
 interface CostProjectionComponentProps {
   data: ReadonlyArray<Report>
@@ -11,17 +12,26 @@ export interface Report {
   readonly totalMonthExpenses: number
 }
 
+const generateRows = (data: ReadonlyArray<Report>) => {
+  let balance = 0
+
+  return data.map(it => {
+
+    balance += it.income - it.totalMonthExpenses
+
+    return {
+      balance,
+      date: it.date,
+      income: it.income,
+      totalMonthExpenses: it.totalMonthExpenses,
+      delta: it.income - it.totalMonthExpenses
+    }
+  })
+}
+
 export const CostsProjectionComponent: React.FC<CostProjectionComponentProps> = (props: CostProjectionComponentProps) => {
 
-  const rows = props.data.map(it => {
-      return {
-        date: it.date,
-        income: it.income,
-        totalMonthExpenses: it.totalMonthExpenses,
-        delta: it.income - it.totalMonthExpenses
-      }
-    }
-  )
+  const rows = generateRows(props.data)
 
   return (
     <TableContainer component={Paper}>
@@ -29,6 +39,7 @@ export const CostsProjectionComponent: React.FC<CostProjectionComponentProps> = 
         <TableHead>
           <TableRow>
             <TableCell>Date</TableCell>
+            <TableCell align="right">Balance</TableCell>
             <TableCell align="right">Income</TableCell>
             <TableCell align="right">Expenses</TableCell>
             <TableCell align="right">Delta</TableCell>
@@ -43,9 +54,10 @@ export const CostsProjectionComponent: React.FC<CostProjectionComponentProps> = 
               <TableCell component="th" scope="row">
                 {row.date.toDateString()}
               </TableCell>
-              <TableCell align="right">{row.income}</TableCell>
-              <TableCell align="right">{row.totalMonthExpenses}</TableCell>
-              <TableCell align="right">{row.delta}</TableCell>
+              <TableCell align="right">{formatNumberToEuro(row.balance)}</TableCell>
+              <TableCell align="right">{formatNumberToEuro(row.income)}</TableCell>
+              <TableCell align="right">{formatNumberToEuro(row.totalMonthExpenses)}</TableCell>
+              <TableCell align="right">{formatNumberToEuro(row.delta)}</TableCell>
             </TableRow>
           ))}
         </TableBody>
