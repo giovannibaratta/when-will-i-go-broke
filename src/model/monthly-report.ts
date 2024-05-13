492153f1f0a2674fafcefe5c2ed273ae0dd82845
Giovanni Baratta <bargiovi@hotmail.it>
2024-05-13 18:25:14 +0200

    Add component and totalIncome to monthlyReport Adapt month attribute type
    Signed-off-by: Giovanni Baratta <bargiovi@hotmail.it>

--- src/model/monthly-report.ts
+++ src/model/monthly-report.ts
@@ -1,10 +1,27 @@
 export interface MonthlyReport {
-  period: Period
-  totalExpenses: number
-  detailedExpenses: Record<string, number>
+  readonly period: Period
+  readonly component: string,
+  readonly totalIncome: number,
+  readonly totalExpenses: number
+  readonly detailedExpenses: Record<string, number>
 }
 
 export interface Period {
-  month: number
+  month: Month
   year: number
+}
+
+export enum Month {
+  January = 0,
+  February = 1,
+  March = 2,
+  April = 3,
+  May = 4,
+  June = 5,
+  July = 6,
+  August = 7,
+  September = 8,
+  October = 9,
+  November = 10,
+  December = 11,
 }
