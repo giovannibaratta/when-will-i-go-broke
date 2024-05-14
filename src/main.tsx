57182db444e1edf3311d44c37c63a212bead78b5
Giovanni Baratta <bargiovi@hotmail.it>
2024-05-14 12:52:35 +0200

    Add redux-persist to persist redux state into local storage
    Signed-off-by: Giovanni Baratta <bargiovi@hotmail.it>

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,20 @@ import ReactDOM from "react-dom/client"
 import App from "./App.tsx"
 import "./index.css"
 import {Provider} from "react-redux"
-import store from "./state/store.ts"
+import store, {persistor} from "./state/store.ts"
 import {LocalizationProvider} from "@mui/x-date-pickers"
 import {AdapterDayjs} from "@mui/x-date-pickers/AdapterDayjs"
+import {PersistGate} from "redux-persist/integration/react"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <App />
-      </LocalizationProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <App />
+        </LocalizationProvider>
+      </PersistGate>
+
     </Provider>
   </React.StrictMode>
 )
--- src/state/store.ts
+++ src/state/store.ts
@@ -1,19 +1,32 @@
-import {configureStore} from "@reduxjs/toolkit"
+import {combineReducers, configureStore} from "@reduxjs/toolkit"
 import carReducer from "./car/car-reducer.ts"
 import {useDispatch, useSelector} from "react-redux"
 import incomeReducer from "./income/income-reducer.ts"
 import settingsReducer from "./settings/settings-reducer.ts"
+import storage from "redux-persist/lib/storage"
+import {persistReducer, persistStore} from "redux-persist"
 
+
+const persistConfig = {
+  key: "root",
+  storage
+}
+
+const reducers = combineReducers({
+  car: carReducer,
+  income: incomeReducer,
+  settings: settingsReducer
+})
+
+const persistedReducer = persistReducer(persistConfig, reducers)
 
 const store = configureStore({
-  reducer: {
-    car: carReducer,
-    income: incomeReducer,
-    settings: settingsReducer
-  }
+  reducer: persistedReducer
 })
 
 export default store
+
+export const persistor = persistStore(store)
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
