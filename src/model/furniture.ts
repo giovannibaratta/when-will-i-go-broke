964fcb1fec72aca8af395cbd41792405933c14d0
Giovanni Baratta <bargiovi@hotmail.it>
2024-05-15 15:19:53 +0200

    Add model to compute furniture costs
    Signed-off-by: Giovanni Baratta <bargiovi@hotmail.it>

---
+++ src/model/furniture.ts
@@ -0,0 +1,75 @@
+import {MonthlyReport, Period} from "./monthly-report.ts"
+import {computeMonthlyPaymentForFixedInterestRateLoan} from "../utils/finance.ts"
+import {addMonthsToPeriod, dateToPeriod, isPeriodBetweenStartAndEnd} from "../utils/date.ts"
+
+interface FurnitureCosts {
+  kitchenCosts: number
+  bathroomCosts: number
+  livingRoomCosts: number
+  bedroomCosts: number
+  loanStartDate: Date
+  loanDurationInMonths: number
+  interestRatePercentage: number
+}
+
+export type FurnitureCostsCalculator = FurnitureCosts & {
+  computeMonthlyReport: (period: Period) => MonthlyReport
+}
+
+export function buildFurnitureExpensesCalculator(config: FurnitureCosts): FurnitureCostsCalculator {
+  return {
+    ...config,
+    computeMonthlyReport: computeMonthlyCosts(config)
+  }
+}
+
+function computeMonthlyCosts(config: FurnitureCosts): (period: Period) => MonthlyReport {
+
+  const {
+    kitchenCosts,
+    bathroomCosts,
+    livingRoomCosts,
+    bedroomCosts,
+    loanStartDate,
+    loanDurationInMonths,
+    interestRatePercentage
+  } = config
+
+  const loanAmount = kitchenCosts + bathroomCosts + livingRoomCosts + bedroomCosts
+
+  const rate = computeMonthlyPaymentForFixedInterestRateLoan({
+    amount: loanAmount,
+    durationInMonths: loanDurationInMonths,
+    annualInterestRateInPercent: interestRatePercentage
+  })
+
+  const startPeriod = dateToPeriod(loanStartDate)
+  const endPeriod = addMonthsToPeriod(startPeriod, loanDurationInMonths)
+
+  return (period: Period): MonthlyReport => {
+    const isLoanOngoing = isPeriodBetweenStartAndEnd(period, startPeriod, endPeriod, {
+      includeStart: true,
+      includeEnd: false
+    })
+
+    const totalExpenses = isLoanOngoing ? rate : 0
+
+    return {
+      period,
+      component: FURNITURE_COMPONENT,
+      totalIncome: 0,
+      totalExpenses,
+      detailedExpenses: {
+        ...(totalExpenses > 0 && {
+          kitchen: kitchenCosts / loanAmount * totalExpenses,
+          bathroom: bathroomCosts / loanAmount * totalExpenses,
+          livingRoom: livingRoomCosts / loanAmount * totalExpenses,
+          bedroom: bedroomCosts / loanAmount * totalExpenses
+        })
+      }
+    }
+
+  }
+}
+
+const FURNITURE_COMPONENT = "Furniture"
