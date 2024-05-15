708f6461f2f8fa2bfd38f15ef8a5d0b463fec5a2
Giovanni Baratta <bargiovi@hotmail.it>
2024-05-15 15:19:24 +0200

    Add isPeriodBetweenStartAndEnd, dateToPeriod, addMonthsToPeriod utility functions
    Signed-off-by: Giovanni Baratta <bargiovi@hotmail.it>

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,3 +1,5 @@
+import {Period} from "../model/monthly-report.ts"
+
 export function getFirstDayOfNextMonthsFrom(now: Date, numberOfMonths: number): Date {
   console.assert(numberOfMonths > 0)
   const currentMonth = now.getMonth()
@@ -8,4 +10,41 @@ export function getFirstDayOfNextMonthsFrom(now: Date, numberOfMonths: number): 
   const newMonth = yearIncrements < 0 ? targetMonth : targetMonth - (12 * yearIncrements)
 
   return new Date(newYear, newMonth, 1)
+}
+
+export function isPeriodBetweenStartAndEnd(period: Period, start: Period, end: Period, config ?: {
+  includeStart: boolean,
+  includeEnd: boolean
+}): boolean {
+
+  const includeStart = config?.includeStart ?? false
+  const includeEnd = config?.includeEnd ?? false
+
+  const isSameAsStart = period.year === start.year && period.month === start.month
+  const isSameAsEnd = period.year === end.year && period.month === end.month
+  const isAfterStart = period.year > start.year || (period.year === start.year && period.month > start.month)
+  const isBeforeEnd = period.year < end.year || (period.year === end.year && period.month < end.month)
+
+  const isBetweenStartAndEnd = (isAfterStart && isBeforeEnd) ||
+    (isSameAsStart && includeStart) ||
+    (isSameAsEnd && includeEnd)
+
+  return isBetweenStartAndEnd
+}
+
+export const dateToPeriod = (date: Date): Period => {
+  return {
+    year: date.getFullYear(),
+    month: date.getMonth()
+  }
+}
+
+export const addMonthsToPeriod = (period: Period, months: number): Period => {
+  const newYear = period.year + Math.floor(months / 12)
+  const newMonth = period.month + months % 12
+
+  return {
+    year: newYear,
+    month: newMonth
+  }
 }
