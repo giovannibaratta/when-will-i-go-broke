811171fee175b469657f02cff6326cb60d1ef189
Giovanni Baratta <bargiovi@hotmail.it>
2024-05-19 11:10:48 +0200

    Add setup for running Jest tests with SWC
    Signed-off-by: Giovanni Baratta <bargiovi@hotmail.it>

---
+++ jest.config.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+  roots: ["<rootDir>/src"],
+  collectCoverageFrom: [
+    "src/**/*.{js,jsx,ts,tsx}",
+    "!src/**/*.d.ts",
+    "!src/mocks/**"
+  ],
+  testEnvironment: "jsdom",
+  modulePaths: ["<rootDir>/src"],
+  transform: {
+    ".*\\.(tsx?)$": [
+      "@swc/jest",
+      {
+        jsc: {
+          transform: {
+            react: {
+              runtime: "automatic"
+            }
+          }
+        }
+      }
+    ]
+  },
+  moduleFileExtensions: ["ts", "tsx", "js"],
+  resetMocks: true,
+  moduleNameMapper: {
+    // Css files need to be handled with the library identity-obj-proxy
+    // Otherwise they will result in errors when running tests
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy"
+  }
+}
