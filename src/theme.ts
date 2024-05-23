0ae824bbf96c2302e89feb8b0909a87d11c4d9ba
Giovanni Baratta <bargiovi@hotmail.it>
2024-05-23 17:02:09 +0200

    Change color of LinkItemText
    Signed-off-by: Giovanni Baratta <bargiovi@hotmail.it>

--- src/theme.ts
+++ src/theme.ts
@@ -4,6 +4,15 @@ const LIGHT_GRAY = "#F5F5F5"
 const DEEP_TEAL = "#0079A1"
 
 export const theme = createTheme({
+  components: {
+    MuiListItemText: {
+      styleOverrides: {
+        root: {
+          color: DEEP_TEAL
+        }
+      }
+    }
+  },
   palette: {
     primary: {
       main: DEEP_TEAL
